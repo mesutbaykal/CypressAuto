1706006f9869fb54d2dd8a5ea8a67b617816e587
mesut.baykal <maxutbykal41@gmail.com>
2023-07-12 00:07:26 +0300

    updated config file

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   e2e: {
     baseUrl: "https://practice.cydeo.com/",
+    video: false,
+    retries: 2,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
