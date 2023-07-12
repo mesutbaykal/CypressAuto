0cd79b53a046f5167ba5769809bb0bb63a450c90
mesut.baykal <maxutbykal41@gmail.com>
2023-07-12 15:02:07 +0300

    eslint-fixed

--- cypress/e2e/testStructure.cy.js
+++ cypress/e2e/testStructure.cy.js
@@ -1,32 +1,32 @@
 /// <reference types="cypress" />
 
-describe("Context: My First Tests", () => {
+describe('Context: My First Tests', () => {
   before(() => {
-    //runs once before all test cases in this describe block,like beforeClass in testNG
+    // runs once before all test cases in this describe block,like beforeClass in testNG
   });
   beforeEach(() => {
-    //run before each test case,beforeMethod in testNG
+    // run before each test case,beforeMethod in testNG
     cy.clearCookies();
   });
   after(() => {
-    //similar to afterClass in TestNG, runs once after all tests finished
+    // similar to afterClass in TestNG, runs once after all tests finished
   });
   afterEach(() => {
-    //similar to afterMethod in TestNG
+    // similar to afterMethod in TestNG
   });
-  it.skip("Open a web application", () => {
-    cy.visit("https://practice.cydeo.com/registration_form");
+  it.skip('Open a web application', () => {
+    cy.visit('https://practice.cydeo.com/registration_form');
   });
-  xit("Test 2", () => {
+  xit('Test 2', () => {
     expect(false).to.equal(false);
   });
-  it("Test 3", () => {
+  it('Test 3', () => {
     expect(false).not.to.equal(true);
   });
-  it("Test 4", () => {
+  it('Test 4', () => {
     expect(5).to.equal(5);
   });
-  it("Test 5", () => {
-    expect(true).to.equal("5" == 5);
+  it('Test 5', () => {
+    expect(true).to.equal('5' == 5);
   });
 });
